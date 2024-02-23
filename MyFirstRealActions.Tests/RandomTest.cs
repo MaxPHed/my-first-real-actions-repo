31f755881016d1cd7e68f5b3257a170f65dd12b3
MaxPHed <114389278+MaxPHed@users.noreply.github.com>
2024-02-23 17:08:47 +0100

    Add workflow

---
+++ MyFirstRealActions.Tests/RandomTest.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+
+namespace MyFirstRealActions.Tests
+{
+    public class RandomTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            string expected = "Hello world";
+
+            expected.Should().BeEquivalentTo(expected);
+        }
+    }
+}
